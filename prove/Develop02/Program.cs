69e79c724f018e1c7346dca97134eb24d0245096
abrahamnyuiadzi <abrahamnyuiadzi@gmail.com>
2024-01-19 12:17:59 +0000

    commit

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        // Create menu and user input variables 
+        Journal   journalGS = new Journal ();
+        int          menuUserinput =0;
+
+       List<string>  menu =new List<string> 
+       {
+          "please select ",
+
+          "1. write ",
+          "2. Display",
+          "3. Load ",
+          "4.Save ",
+          "5. quit ",
+       " what do you to do ?"
+
+       };
+
+       while (menuUserinput!= 5)
+       {
+        foreach(string menuItem in menu ){
+            Console.WriteLine(menuItem);
+            
+        }
+
+        menuUserinput =int.Parse(Console.ReadLine());
+       }
+
+       //while user input is not 5
+
+
+       //stwitch statement for the user .
     }
 }
