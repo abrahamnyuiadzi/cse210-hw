7b42d5e8a44b58760002c6fa45d0d3e2ccf41992
abrahamnyuiadzi <abrahamnyuiadzi@gmail.com>
2024-01-29 10:51:56 +0000

    week3

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,16 @@
+using System;
+ public class Scripture{
+
+
+private List<Scripture> _Words = new List<Scripture> ();
+
+private Reference _Reference;
+
+private Scripture(){
+    _Reference =null;
+    _Words=null;
+}
+
+
+
+ }
