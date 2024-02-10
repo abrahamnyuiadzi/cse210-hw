2527cd8423a072717a6c5ab28aa43c35dc909a52
abrahamnyuiadzi <abrahamnyuiadzi@gmail.com>
2024-02-10 11:28:23 +0000

    commit 3

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,10 +5,14 @@ class Program
     static void Main(string[] args)
     {
        
-         System.Console.WriteLine("please enter to continue or quit  to finish  ");
+
+         System.Console.WriteLine("For God so loved the world that he gave his one and only Son," );
+         System.Console.WriteLine("that whoever believes in him shall not perish but have eternal life.");
+      System.Console.WriteLine("................................................................");
+
+
+   System.Console.WriteLine("please enter to continue or quit  to finish  ");
+  
        
-
-
-        
-    }
+}
 }
