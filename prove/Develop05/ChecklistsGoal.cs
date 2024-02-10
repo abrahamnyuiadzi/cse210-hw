732dc00e6ac066be177cf3453a9e331967e2c3cb
abrahamnyuiadzi <abrahamnyuiadzi@gmail.com>
2024-02-10 19:49:32 +0000

    week 5

---
+++ prove/Develop05/ChecklistsGoal.cs
@@ -0,0 +1,16 @@
+class CheckListsGoal{
+
+    
+   string filename = "myFile.txt";
+string[] lines = System.IO.File.ReadAllLines(filename);
+
+foreach (string line in lines)
+{
+    string[] parts = line.Split(",");
+
+    string firstName = parts[0];
+    string lastName = parts[1];
+
+
+} 
+}
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+           Console.WriteLine(@"what is your choose between 1-5);
+        .......1.create new goal
+        ........2.List goals
+        ........3.Save goal
+        ........4 load goal
+        ........5.record Event
+        ");
+
+          System.Console.WriteLine("what is your choice ");
+        String userInput =Console.ReadLine();
+
     }
 }
